34359bc8d2c4ccf588b88e68432269de6bc22e6f
martyn <martyn@threephasephoto.co.uk>
2021-12-12 15:42:18 +0000

    Adding check intervals and code comments overall

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ requires Node v16 or newer
 
 clone repo and run npm install in the folder to install dependencies
 
-copy/rename the .env.template file as .env and provide keys/channels/collection codes for the bot to monitor
+copy/rename the .env.template file as .env and provide keys/channels/collection codes for the bot to monitor, you can also adjust the checking interval (in seconds)
 
 run using "node bot.js" from command line
--- bot.js
+++ bot.js
@@ -1,73 +1,84 @@
-require("dotenv").config();
-const { Client, Intents } = require("discord.js");
-const axios = require("axios");
-axios.defaults.headers.common["X-API-KEY"] = process.env.openseaKey;
-axios.defaults.headers.common["Accept"] = "application/json";
+require("dotenv").config(); // pull in our variables from the .env file
+const { Client, Intents } = require("discord.js"); // setup bits of discord we require
+const axios = require("axios"); // used for making requests to openseas API
+axios.defaults.headers.common["X-API-KEY"] = process.env.openseaKey; // set our opensea API token
+axios.defaults.headers.common["Accept"] = "application/json"; // we want opensea to respond in JSON
 
 const botIntents = new Intents();
 botIntents.add(Intents.FLAGS.GUILD_MESSAGES);
-let channel;
+
+let channel; // storage variable for our discord channel to talk into
+let lastChecked = new Date(); // storage variable for the last time we checked opensea for new transactions
+
 function runBot() {
+  // initial bot setup function which gets run on startup
   const client = new Client({
+    // create a bot client configuration
     intents: botIntents,
     partials: ["CHANNEL", "MESSAGE"],
   });
   client.on("ready", async () => {
-    console.log("Logged in as " + client.user.tag);
-    channel = await client.channels.fetch(process.env.channelId);
-    await watchOpensea();
-    process.exit();
-    // setInterval(() => {
-    //   watchOpensea();
-    // }, 30000);
+    // setup actions to take once the bot connects including to:
+    console.log("Logged in as " + client.user.tag); // log in console that the bot has correctly logged in
+    channel = await client.channels.fetch(process.env.channelId); // set our storage variable for our announcement channel
+
+    setInterval(() => {
+      // setup a timer which triggers on our intervals to do our announcements
+      watchOpensea(); // announcement function call
+    }, Number(process.env.checkInterval) * 1000);
   });
-  client.login(process.env.botToken);
+  client.login(process.env.botToken); // tell the bot to try to log in now we have set up its actions
 }
 
-let lastChecked = new Date();
-
 async function watchOpensea() {
+  // announcement function
   try {
     let eventsQuery = await axios.get(
+      // get data for any new sales since the last time we checked
       "https://api.opensea.io/api/v1/events?collection_slug=" +
         process.env.collection_slug +
         "&event_type=successful&only_opensea=true&occurred_after=" +
-        Math.floor((lastChecked.getTime() - 3 * 86400000) / 1000)
+        Math.floor(lastChecked.getTime() / 1000)
     );
 
     let price, usdPrice, currency;
+    // loop thorugh sales and announce them
     for (let token of eventsQuery.data.asset_events) {
+      // do some basic calcs to make our life more readable below...
       price = token.total_price / Math.pow(10, token.payment_token.decimals);
       usdPrice = Math.round(price * token.payment_token.usd_price * 100) / 100;
       currency = token.payment_token.symbol;
       await channel.send({
+        // send announcement to channel
         embeds: [
           {
-            url: token.asset.permalink,
-            title: "Sold " + token.asset.name + "!",
+            url: token.asset.permalink, // link to the token permalink
+            title: "Sold " + token.asset.name + "!", // title text
             description:
-              "**Price:** " +
+              "**Price:** " + // price info
               price +
               currency +
               " ($" +
               usdPrice +
               ")\n**Seller:** " +
-              token.seller.user.username +
+              (token.seller.user.username // sometimes the buyer/sellers name is not known and would display as "null", this makes it read as "Unknown" instead, looks prettier
+                ? token.seller.user.username
+                : "Unknown") +
               "\n**Buyer:** " +
-              (token.winner_account.user.username
+              (token.winner_account.user.username // same prettification as above
                 ? token.winner_account.user.username
                 : "Unknown"),
             image: {
-              url: token.asset.image_original_url,
+              url: token.asset.image_original_url, // include image of the token
             },
           },
         ],
       });
     }
 
-    lastChecked = new Date();
+    lastChecked = new Date(); // update our last checked date for next time we are called
   } catch (err) {
     console.log(err);
   }
 }
-runBot();
+runBot(); // start!
